fba6a6e609350f12172bfce510c4faaec5bc0197
bhupendra-64 <bhupendras.ug24.ce@nitp.ac.in>
2025-03-07 22:42:34 +0530

    css updated

--- OmniGen/src/assets/pages/Home.jsx
+++ OmniGen/src/assets/pages/Home.jsx
@@ -81,7 +81,7 @@ const Home = () => {
         
 
         <br /> <br />  <br /> <hr /> <br /> <br /> 
-        <a id="contact" href='https://contact-us-omenigen.netlify.app/' target='_main'>Your Query </a>
+        <a id="contact" href='https://contact-us-omnigen.netlify.app/' target='_main'>Your Query </a>
         <a id="contact" href='https://www.instagram.com/sachinnnn_64?igsh=YTRiYjI1ZTh4dHdi' target='_main'>   | Contact Us</a>
         <br /> 
         <br /> 
